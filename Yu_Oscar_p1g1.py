914a943515d0e377436016cdeea365f969e0d642
oscar yu <oscaryu333@gmail.com>
2021-09-23 11:18:17 -0700

    added codes from python

--- Yu_Oscar_p1g1.py
+++ Yu_Oscar_p1g1.py
@@ -4,3 +4,17 @@
 #It will keep track of score and give
 #a final result
 
+import time
+score = "Diamonds"
+points = score
+print("welcome to the quiz")
+time.sleep (5)
+print("What is your name?")
+
+user_name = input()
+time.sleep(2)
+print(f"welcome {user_name}")
+time.sleep(2)
+print("Your objective is to answer all the questions correctly.")
+
+
