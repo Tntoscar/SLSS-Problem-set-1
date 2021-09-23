7552767996c16ba5f04e0a69d921abd6841d8fef
oscar yu <oscaryu333@gmail.com>
2021-09-23 12:15:42 -0700

    added more codes

---
+++ Yu_Oscar_p1final.py
@@ -0,0 +1,23 @@
+# Quiz Creation Activity
+
+#Quiz has 5 questions the user will answer.
+#It will keep track of score and give
+#a final result
+
+import time
+score = 0
+score = "diamonds"
+print("welcome to the quiz")
+time.sleep (5)
+print("What is your name?")
+
+user_name = input()
+time.sleep(2)
+print(f"welcome {user_name}")
+time.sleep(2)
+print("Your objective is to answer all the questions correctly.")
+time.sleep(2)
+print("lets begin")
+time.sleep(2)
+print("Who was the first Avenger?")
+
